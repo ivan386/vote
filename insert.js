bd853e102cfc5ca233b33fa6c751d096bd07ed27
ivan386 <ivan386@yandex.ru>
2016-08-02 16:59:04 +0400

    Совместимость с Greasemonkey и Tampermonkey

--- insert.js
+++ insert.js
@@ -1,5 +1,5 @@
 var script   = document.createElement('script');
 script.type  = 'text/javascript';
-script.src   = chrome.extension.getURL("script.js");
+script.src   = chrome.extension.getURL("script.user.js");
 script.async = 1;
 document.head.appendChild(script);
